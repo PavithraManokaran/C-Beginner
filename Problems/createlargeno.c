6047afcebc75a581bf20b1f6295da1c2bd865cc4
PavithraManokaran <34641765+PavithraManokaran@users.noreply.github.com>
2018-05-16 18:27:06 +0530

    create createlargeno.c

---
+++ Problems/createlargeno.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+int n,a[25],i,j,t,count=0;
+printf("\nEnter the how much numbers:");
+scanf("\n%d",&n);
+printf("\nEnter the numbers:");
+for(i=0;i<n;i++)
+{
+   scanf("%d",&a[i]); 
+}
+for(i=0;i<n;i++)
+{
+    for(j=i+1;j<n;j++)
+    if(a[i]<a[j])
+    {
+       t=a[i];
+       a[i]=a[j];
+       a[j]=t;
+    }
+}
+for(i=0;i<n;i++)
+{
+   printf("%d",a[i]); 
+}
+return 0;
+}
